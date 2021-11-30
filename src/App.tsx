e2a7f3febbc57847f6f4dc4d770fd9e557ea6a98
Sam Sloate <sam.sloate@appian.com>
2021-11-30 12:10:57 -0500

    Update the event listings

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,10 @@ function App() {
       {!flipped && <a.div style={{ opacity: opacity.to(o => 1 - o), transform }}>
         <Schedule
           events={[
-            { time: "4:00:00", description: "Rehearsal Dinner at the pool" },
+            { time: "6:00:00", description: "Cocktail Hour" },
+            { time: "7:00:00", description: "Rehearsal Dinner Bergins" },
+            { time: "7:30:00", description: "Food is served" },
+            { time: "10:00:00", description: "End of Dinner" },
           ]}
           date="March 31st, 2021"
         ></Schedule>
@@ -30,7 +33,19 @@ function App() {
         }}
       >
         <Schedule
-          events={[{ time: "5:00:00", description: "Wedding Begins" }]}
+          events={[
+            { time: "5:00:00", description: "Ceremony" },
+            { time: "5:45:00", description: "Cocktails and Canapes" },
+            { time: "6:50:00", description: "Introductions" },
+            { time: "7:00:00", description: "We dance" },
+            { time: "7:15:00", description: "We dine" },
+            { time: "7:50:00", description: "Speaches" },
+            { time: "8:20:00", description: "Cut Cake" },
+            { time: "8:45:00", description: "We Party" },
+            { time: "11:00:00", description: "First Bus Home" },
+            { time: "12:00:00", description: "Second Bus Home" },
+            { time: "2:00:00", description: "Last Bus Home" },
+          ]}
           date="April 1st, 2021"
         ></Schedule>
       </a.div>
