e25a55396f15299097c1df754867733e5e2fb9a7
Sam Sloate <sam.sloate@appian.com>
2021-11-30 13:18:14 -0500

    Fix up some styling

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <Schedule
           events={[
             { time: "6:00:00", description: "Cocktail Hour" },
-            { time: "7:00:00", description: "Rehearsal Dinner Bergins" },
+            { time: "7:00:00", description: "Rehearsal Dinner Begins" },
             { time: "7:30:00", description: "Food is served" },
             { time: "10:00:00", description: "End of Dinner" },
           ]}
@@ -39,9 +39,9 @@ function App() {
             { time: "6:50:00", description: "Introductions" },
             { time: "7:00:00", description: "We dance" },
             { time: "7:15:00", description: "We dine" },
-            { time: "7:50:00", description: "Speaches" },
-            { time: "8:20:00", description: "Cut Cake" },
-            { time: "8:45:00", description: "We Party" },
+            { time: "7:50:00", description: "We speak" },
+            { time: "8:20:00", description: "We cut cake" },
+            { time: "8:45:00", description: "We party" },
             { time: "11:00:00", description: "First Bus Home" },
             { time: "12:00:00", description: "Second Bus Home" },
             { time: "2:00:00", description: "Last Bus Home" },
